c2c3fe6ed50a27d38717648466c2561dbb8a8e21
Leonardo Loayza <leonardoloayza28@gmail.com>
2023-03-03 20:24:56 -0500

    I made an error it should have been serializers.ModelSerializer in the MovieSerializer class in the file serializer.py

--- movie/urls.py
+++ movie/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from rest_framework import routers 
 from . import views 
 
+#To be able to use the MovieViewSet we need to use a router 
 router = routers.DefaultRouter()
 router.register('', views.MovieViewSet) 
 
