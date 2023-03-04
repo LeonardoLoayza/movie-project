566eea5cec2af5ceed4bb4986c8a14bb7e207475
Leonardo Loayza <leonardoloayza28@gmail.com>
2023-03-03 20:19:59 -0500

    to use our class viewset we need to create a router,etc so I modified the urls.py to make it happen

--- movie/urls.py
+++ movie/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path 
+from django.urls import path, include
+from rest_framework import routers 
 from . import views 
 
+router = routers.DefaultRouter()
+router.register('', views.MovieViewSet) 
+
 urlpatterns = [
-    path('', views.index)
+    path('', include(router.urls))
 ]
+
 
